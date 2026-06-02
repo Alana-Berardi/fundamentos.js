9cef9a4c00cf2c9d9eadc9b7bd11aa7c4def3cdd
Alana-Berardi <alana.berardi.santos@escola.pr.gov.br>
2026-06-02 13:34:05 +0000

    aula03

---
+++ Arrays/procurando-aluno.js
@@ -0,0 +1,15 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+
+const medias = [10, 0, 7.3, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeNota(alunos){
+       if(listaDeAlunosEMedias[0] .includes(aluno)){
+            console. log('${aluno} está cadastrado! ' ) ;
+       }else{
+            console. log('${aluno} está cadastrado! ' ) ;
+       }
+}
+
+exibeNomeENota("João");
